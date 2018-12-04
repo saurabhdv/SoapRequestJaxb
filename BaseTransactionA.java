c91f57656de3aee9a52479e9dcd8818b59eaae10
SaurabhDVerma <sauabh305@gmail.com>
2018-12-04 17:36:22 +0530

    this is to read soap ui

---
+++ BaseTransactionA.java
@@ -0,0 +1,164 @@
+package com.Actimize.pojo;
+
+public class BaseTransactionA {
+
+	 private String actimizeInputFileName;
+	 private String actionInitiatorCd;
+	 private String branchKey;
+	 private String cardKey;
+	 private String channel;
+	 private String dataSetId;
+	 private String employeeKey;
+	 private String executingTerminalId;
+	 private String executingUserId;
+	 private String isOpenBankingGatewayTransaction;
+	 private String partyKey;
+	 private String productBrand;
+	 private String resendDueToFailureInd;
+	 private String sessionKey;
+	 private String sourceCd;
+	 private String tenantId;
+	 private String transactionLocalDateTime;
+	 private String transactionNormalizedDateTime;
+	 private String transactionSECCode;
+	 private String transactionSendingSystemCd;
+	 private String transferApprovingUserId;
+	 private String userId;
+	 
+	 
+	
+	public String getActimizeInputFileName() {
+		return actimizeInputFileName;
+	}
+	public void setActimizeInputFileName(String actimizeInputFileName) {
+		this.actimizeInputFileName = actimizeInputFileName;
+	}
+	public String getActionInitiatorCd() {
+		return actionInitiatorCd;
+	}
+	public void setActionInitiatorCd(String actionInitiatorCd) {
+		this.actionInitiatorCd = actionInitiatorCd;
+	}
+	public String getBranchKey() {
+		return branchKey;
+	}
+	public void setBranchKey(String branchKey) {
+		this.branchKey = branchKey;
+	}
+	public String getCardKey() {
+		return cardKey;
+	}
+	public void setCardKey(String cardKey) {
+		this.cardKey = cardKey;
+	}
+	public String getDataSetId() {
+		return dataSetId;
+	}
+	public void setDataSetId(String dataSetId) {
+		this.dataSetId = dataSetId;
+	}
+	public String getEmployeeKey() {
+		return employeeKey;
+	}
+	public void setEmployeeKey(String employeeKey) {
+		this.employeeKey = employeeKey;
+	}
+	public String getExecutingTerminalId() {
+		return executingTerminalId;
+	}
+	public void setExecutingTerminalId(String executingTerminalId) {
+		this.executingTerminalId = executingTerminalId;
+	}
+	public String getExecutingUserId() {
+		return executingUserId;
+	}
+	public void setExecutingUserId(String executingUserId) {
+		this.executingUserId = executingUserId;
+	}
+	public String getIsOpenBankingGatewayTransaction() {
+		return isOpenBankingGatewayTransaction;
+	}
+	public void setIsOpenBankingGatewayTransaction(String isOpenBankingGatewayTransaction) {
+		this.isOpenBankingGatewayTransaction = isOpenBankingGatewayTransaction;
+	}
+	public String getProductBrand() {
+		return productBrand;
+	}
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+	public String getResendDueToFailureInd() {
+		return resendDueToFailureInd;
+	}
+	public void setResendDueToFailureInd(String resendDueToFailureInd) {
+		this.resendDueToFailureInd = resendDueToFailureInd;
+	}
+	public String getSourceCd() {
+		return sourceCd;
+	}
+	public void setSourceCd(String sourceCd) {
+		this.sourceCd = sourceCd;
+	}
+	public String getTenantId() {
+		return tenantId;
+	}
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+	public String getTransactionLocalDateTime() {
+		return transactionLocalDateTime;
+	}
+	public void setTransactionLocalDateTime(String transactionLocalDateTime) {
+		this.transactionLocalDateTime = transactionLocalDateTime;
+	}
+	public String getTransactionSECCode() {
+		return transactionSECCode;
+	}
+	public void setTransactionSECCode(String transactionSECCode) {
+		this.transactionSECCode = transactionSECCode;
+	}
+	public String getTransactionSendingSystemCd() {
+		return transactionSendingSystemCd;
+	}
+	public void setTransactionSendingSystemCd(String transactionSendingSystemCd) {
+		this.transactionSendingSystemCd = transactionSendingSystemCd;
+	}
+	public String getTransferApprovingUserId() {
+		return transferApprovingUserId;
+	}
+	public void setTransferApprovingUserId(String transferApprovingUserId) {
+		this.transferApprovingUserId = transferApprovingUserId;
+	}
+	public String getChannel() {
+		return channel;
+	}
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+	public String getPartyKey() {
+		return partyKey;
+	}
+	public void setPartyKey(String partyKey) {
+		this.partyKey = partyKey;
+	}
+	public String getTransactionNormalizedDateTime() {
+		return transactionNormalizedDateTime;
+	}
+	public void setTransactionNormalizedDateTime(String transactionNormalizedDateTime) {
+		this.transactionNormalizedDateTime = transactionNormalizedDateTime;
+	}
+	public String getSessionKey() {
+		return sessionKey;
+	}
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
